693e044094d4d42d22d1656dd6e3c68fecef1244
poltorina <poltorina96@gmail.com>
2018-04-10 15:00:32 +0300

    debounce all request to TV and some small fix

--- youtubePlay/js/script.js
+++ youtubePlay/js/script.js
@@ -1,6 +1,6 @@
 const lolalStorFunc = (param, keyValue, callback) => {
     chrome.storage.local[param](keyValue, result => {
-      param === 'set' ? console.log('Settings saved') : constLocalObj(result);
+      param === 'set' ? console.log('Settings saved', keyValue) : constLocalObj(result);
       if (callback) callback();
     });
   },
@@ -13,7 +13,7 @@ const lolalStorFunc = (param, keyValue, callback) => {
 
 function debounce(func, wait, immediate) {
   let timeout;
-  return () => {
+  return function () {
     const context = this, args = arguments;
     const later = () => {
       timeout = null;
@@ -26,27 +26,67 @@ function debounce(func, wait, immediate) {
   };
 }
 
+function sendToTV(cmd, href) {
+  if (cmd.action === 'start') {
+    lolalStorFunc('set', {'videoPlay': href ? href : location.href});
+    changeState('load');
+  }
+  chrome.extension.sendMessage({
+    href: href ? href : location.href,
+    msg: 'action',
+    cmd
+  });
+}
 
-lolalStorFunc('get', ['hostname', 'isPlaying', 'needPlayVideo'], () => {
-  if (localObj.needPlayVideo)
-    sendToTV({
-      action: 'start',
-      num: getCurrentPercent()
-    })
-});
-lolalStorFunc('set', {'isPlaying': false});
+function changeState(stateBack) {
+  state.play = (stateBack === 'play' || stateBack === 'load');
+  let play = (stateBack === 'play' || stateBack === 'load');
+  docQ('.html5-video-container').style.opacity = play ? 0 : 1;
+  docQ('.yt-airplay-layer').style.opacity = play ? 1 : 0;
+  docQ(".video-stream.html5-main-video").muted = play;
+  docQ(".ytp-mute-button.ytp-button").disabled = play;
+  docQ(".ytp-mute-button.ytp-button").innerHTML = play ?
+    '<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytp-id-65"></use><path class="ytp-svg-fill" d="m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z" id="ytp-id-65"></path></svg>'
+    : '<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytp-id-15"></use><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytp-id-16"></use><defs><clipPath id="ytp-svg-volume-animation-mask"><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path class="ytp-svg-volume-animation-mover" d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z" transform="translate(0, 0)"></path></clipPath><clipPath id="ytp-svg-volume-animation-slash-mask"><path class="ytp-svg-volume-animation-mover" d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path class="ytp-svg-fill ytp-svg-volume-animation-speaker" clip-path="url(#ytp-svg-volume-animation-mask)" d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z" fill="#fff" id="ytp-id-15"></path><path class="ytp-svg-fill ytp-svg-volume-animation-hider" clip-path="url(#ytp-svg-volume-animation-slash-mask)" d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff" id="ytp-id-16" style="display: none;"></path></svg>'
+
+  console.log('stateBack', stateBack);
+
+  docQ('.yt-airplay-layer .airplay-loading').style.display = stateBack === 'play' ? 'none' : 'flex';
+  docQ('.yt-airplay-layer .airplay-play').style.display = stateBack === 'play' ? 'flex' : 'none';
+
+  lolalStorFunc('set', {'isPlaying': stateBack === 'play'});
+
+  if (stateBack === 'stop') lolalStorFunc('set', {'needPlayVideo': false});
+  if (stateBack === 'load') if (docQ('.ytp-play-button[aria-label="Pause"]')) docQ('.ytp-play-button[aria-label="Pause"]').click();
+  if (stateBack === 'play') if (docQ('.ytp-play-button[aria-label="Play"]')) docQ('.ytp-play-button[aria-label="Play"]').click();
+}
+
+const sendDebounce = {
+  scrub: debounce(() =>
+      sendToTV({
+        action: 'scrub',
+        num: getCurrentTime()
+      })
+    , 500),
+  pause: debounce(() =>
+      sendToTV({
+        action: 'pause',
+        num: 0
+      })
+    , 500),
+  start: debounce(e =>
+      sendToTV({
+        action: 'start',
+        num: getCurrentPercent()
+      }, e)
+    , 500)
+};
 
 const getCurrentTime = () => parseInt(docQ('.ytp-progress-bar').getAttribute('aria-valuenow'));
 const getCurrentPercent = () => {
   const value = v => parseInt(docQ('.ytp-progress-bar').getAttribute(`aria-${v}`));
   return (value('valuenow') * 100) / value('valuemax') / 100;
 };
-const setTime = debounce(() =>
-    sendToTV({
-      action: 'scrub',
-      num: getCurrentTime()
-    })
-  , 500);
 const inputChange = debounce(() => {
     let value = docQ('.yt-airplay-ext-popup .popup-select input').value;
     docQ('.yt-airplay-ext-popup .popup-select span').innerText = value;
@@ -54,6 +94,12 @@ const inputChange = debounce(() => {
   }
   , 500);
 let state = {};
+
+lolalStorFunc('get', ['hostname', 'isPlaying', 'needPlayVideo'], () => {
+  console.log(localObj.needPlayVideo);
+  if (localObj.needPlayVideo) sendDebounce.start();
+});
+lolalStorFunc('set', {'isPlaying': false});
 
 function init() {
   const play_pause = docQ('.ytp-play-button.ytp-button');
@@ -223,10 +269,7 @@ function init() {
     listen('.yt-airplay-ext-popup .popup-select span', 'click', () => {
       removeContextMenu();
       lolalStorFunc('set', {'isPlaying': true});
-      sendToTV({
-        action: 'start',
-        num: getCurrentPercent()
-      });
+      sendDebounce.start();
     });
   }
 
@@ -333,43 +376,27 @@ function init() {
 
   createBtn();
 
-  console.log('new init()!!!!', localObj);
-  // debugger
-  if (localObj.needPlayVideo)
-    sendToTV({
-      action: 'start',
-      num: getCurrentPercent()
-    });
+  console.log('new init()!!!!', JSON.stringify(localObj));
+  if (localObj.needPlayVideo) sendDebounce.start();
 
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       if (mutation.type === "attributes") {
-        // console.log('\n\n\n', 'attributes changed', mutation, '\n\n\n\n');
-
-        lolalStorFunc('get', ['hostname', 'isPlaying', 'needPlayVideo'], () => {
+        lolalStorFunc('get', ['hostname', 'isPlaying', 'needPlayVideo', 'videoPlay'], () => {
           if (localObj.isPlaying) {
             const classL = mutation.target.classList;
 
-            if (classL.contains('ytp-play-button')) {
-              sendToTV({
-                action: play_pause.getAttribute('aria-label') === 'Play' ? 'pause' : 'scrub',
-                num: getCurrentTime()
-              })
-            }
+            if (classL.contains('ytp-play-button'))
+              play_pause.getAttribute('aria-label') === 'Play' ? sendDebounce.pause() : (localObj.videoPlay === location.href ? sendDebounce.scrub() : sendDebounce.start());
+
             else if (classL.contains('ytp-upnext') && classL.contains('ytp-suggestion-set')) {
               let click;
               next.addEventListener('click', e => {
                 click = true;
-                sendToTV({
-                  action: 'start',
-                  num: getCurrentPercent()
-                }, e.target.href)
+                sendDebounce.start(e.target.href)
               });
               setTimeout(function () {
-                if (!click) sendToTV({
-                  action: 'start',
-                  num: getCurrentPercent()
-                })
+                if (!click) sendDebounce.start();
               }, 10000)
             }
             else if (classL.contains('html5-video-player')) {
@@ -377,14 +404,11 @@ function init() {
 
               if (videoWasEnd && classL.contains('playing-mode')) {
                 videoWasEnd = false;
-                sendToTV({
-                  action: 'start',
-                  num: getCurrentPercent()
-                })
+                sendDebounce.start();
               }
             }
             else if (classL.contains('ytp-bezel')) {
-              if (mutation.target.getAttribute('aria-label') === '') setTime()
+              if (mutation.target.getAttribute('aria-label') === '') sendDebounce.scrub()
             }
             else if (classL.contains('ytp-volume-panel')) {
               sendToTV({
@@ -407,41 +431,7 @@ function init() {
     el.addEventListener('click', () => lolalStorFunc('set', {'needPlayVideo': true}))
   );
 
-  next.addEventListener('click', e =>
-    sendToTV({
-      action: 'start',
-      num: '0'
-    }, e.target.href)
-  );
-}
-
-function sendToTV(cmd, href) {
-  if (cmd.action === 'start') changeState('load');
-  chrome.extension.sendMessage({
-    href: href ? href : location.href,
-    msg: 'action',
-    cmd
-  });
-}
-
-function changeState(stateBack) {
-  state.play = (stateBack === 'play' || stateBack === 'load');
-  let play = (stateBack === 'play' || stateBack === 'load');
-  docQ('.html5-video-container').style.opacity = play ? 0 : 1;
-  docQ('.yt-airplay-layer').style.opacity = play ? 1 : 0;
-  docQ(".video-stream.html5-main-video").muted = play;
-  docQ(".ytp-mute-button.ytp-button").disabled = play;
-  docQ(".ytp-mute-button.ytp-button").innerHTML = play ?
-    '<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytp-id-65"></use><path class="ytp-svg-fill" d="m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z" id="ytp-id-65"></path></svg>'
-    : '<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytp-id-15"></use><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytp-id-16"></use><defs><clipPath id="ytp-svg-volume-animation-mask"><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path class="ytp-svg-volume-animation-mover" d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z" transform="translate(0, 0)"></path></clipPath><clipPath id="ytp-svg-volume-animation-slash-mask"><path class="ytp-svg-volume-animation-mover" d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path class="ytp-svg-fill ytp-svg-volume-animation-speaker" clip-path="url(#ytp-svg-volume-animation-mask)" d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z" fill="#fff" id="ytp-id-15"></path><path class="ytp-svg-fill ytp-svg-volume-animation-hider" clip-path="url(#ytp-svg-volume-animation-slash-mask)" d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff" id="ytp-id-16" style="display: none;"></path></svg>'
-
-  console.log('stateBack', stateBack);
-
-  docQ('.yt-airplay-layer .airplay-loading').style.display = stateBack === 'play' ? 'none' : 'flex';
-  docQ('.yt-airplay-layer .airplay-play').style.display = stateBack === 'play' ? 'flex' : 'none';
-
-  lolalStorFunc('set', {'isPlaying': stateBack === 'play'});
-  if(stateBack === 'stop') lolalStorFunc('set', {'needPlayVideo': false});
+  next.addEventListener('click', e => sendDebounce.start(e.target.href));
 }
 
 chrome.runtime.onMessage.addListener((obj) => {
